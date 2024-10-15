dc79382bb4c7212ee0db226511429258ea3091b0
Sheikhmsip <sheikhmsip@gmail.com>
2024-10-16 01:24:54 +0600

    added footer layout for design

--- visitor-management-client/src/Pages/Home/Home.jsx
+++ visitor-management-client/src/Pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import About from '../About/About';
+import React from "react";
+import Banner from "../Banner/Banner";
+import About from "../About/About";
+import Footer from "../Shared/Footer/Footer";
 
 const Home = () => {
-    return (
-        <div className="bg-gradient-to-r from-black via-blue-950 to-purple-950 text-white min-h-[100vh]">
-            <Banner></Banner>
-            <About></About>
-        </div>
-    );
+  return (
+    <div className="bg-gradient-to-r from-black via-blue-950 to-purple-950 text-white min-h-[100vh]">
+      <Banner></Banner>
+      <About></About>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
